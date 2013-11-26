c39a5fe95183b36072d300db7011df65c441d7f9
Elvis Nunez <elvisnunez@me.com>
2013-11-26 12:41:31 +0100

    Initial Commit

---
+++ Thyme/HYPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HYPAppDelegate.h
+//  Thyme
+//
+//  Created by Elvis Nunez on 26/11/13.
+//  Copyright (c) 2013 Hyper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HYPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Thyme/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Thyme
+//
+//  Created by Elvis Nunez on 26/11/13.
+//  Copyright (c) 2013 Hyper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HYPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HYPAppDelegate class]));
+    }
+}
