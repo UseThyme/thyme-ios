f7592050aa789f7012f4d6f3b82cfbe4afe8e3b8
Elvis Nunez <elvisnunez@me.com>
2014-04-01 19:06:49 +0200

    Prepare 1.1
    Added Icons, Launch images and iOS 6 Support

---
+++ Thyme/External/UIScreen-ANDYResolutions/UIScreen+ANDYResolutions.h
@@ -0,0 +1,12 @@
+//
+//  UIScreen+ANDYResolutions.h
+//
+//  Created by Elvis Nunez on 2/13/14.
+//  Copyright (c) 2014 Elvis Nuñez. All rights reserved.
+//
+
+@interface UIScreen (ANDYResolutions)
++ (BOOL)andy_isPhone;
++ (BOOL)andy_isPad;
++ (BOOL)andy_isSmallScreen;
+@end
