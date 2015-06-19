97e3b76f86dd1c83db59b6dfe6c0a717c11657f1
Christoffer Winterkvist <chris@zenangst.com>
2015-06-19 09:31:25 +0200

    Remove HYPHomeViewController

--- Thyme/AppDelegate/Thyme-Bridging-Header.h
+++ Thyme/AppDelegate/Thyme-Bridging-Header.h
@@ -2,7 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "HYPHomeViewController.h"
 #import "HYPTimerViewController.h"
 #import <HockeySDK/HockeySDK.h>
 #import "HYPAlarm.h"
