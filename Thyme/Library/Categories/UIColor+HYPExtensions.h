8d2c095a26de2ab04ec2bc42e2c88050b104e524
Christoffer Winterkvist <chris@zenangst.com>
2015-08-24 19:02:44 +0200

    Remove UIColor+HYPExtensions.m

--- Thyme/Library/Categories/UIColor+HYPExtensions.h
+++
@@ -1,9 +0,0 @@
-@import UIKit;
-
-@interface UIColor (HYPExtensions)
-
-+ (UIColor *)colorFromHexString:(NSString *)hexString;
-
-+ (UIColor *)colorWithRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue;
-
-@end
