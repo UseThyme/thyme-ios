17f623536d1d1617fc35e768f8a48e0ea562c2a0
Christoffer Winterkvist <chris@zenangst.com>
2015-08-24 18:50:54 +0200

    Add Screen struct

---
+++ Thyme/Library/Structs/Screen.swift
@@ -0,0 +1,13 @@
+//
+//  Screen.swift
+//  Thyme
+//
+//  Created by Christoffer Winterkvist on 8/24/15.
+//  Copyright (c) 2015 Hyper. All rights reserved.
+//
+
+import UIKit
+
+struct Screen {
+    
+}
