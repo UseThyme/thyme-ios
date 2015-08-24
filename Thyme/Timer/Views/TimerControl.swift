3d4bdca713f7c615d8cb38e917577a2fe1790026
Christoffer Winterkvist <chris@zenangst.com>
2015-08-24 19:28:46 +0200

    Move draw methods to extension on TimerControl

--- Thyme/Timer/Views/TimerControl.swift
+++ Thyme/Timer/Views/TimerControl.swift
@@ -292,67 +292,6 @@ public class TimerControl: UIControl {
     }
   }
 
-  func drawCircle(context: CGContextRef, color: UIColor, rect: CGRect) {
-    CGContextSaveGState(context)
-    color.set()
-    CGContextFillEllipseInRect(context, rect)
-    CGContextRestoreGState(context)
-  }
-
-  func drawMinutes(context: CGContextRef, color: UIColor, radius: CGFloat, angle: CGFloat, containerRect: CGRect) {
-    CGContextSaveGState(context)
-
-    let angleTranslation: CGFloat = 0 - 90
-    let startDeg: CGFloat = π * (0 + angleTranslation) / 180
-    let endDeg: CGFloat = π * (angle + angleTranslation) / 180
-    let x = CGRectGetWidth(containerRect) / 2 + containerRect.origin.x
-    let y = CGRectGetWidth(containerRect) / 2 + containerRect.origin.y
-
-    color.set()
-
-    CGContextMoveToPoint(context, x, y)
-    CGContextAddArc(context, x, y, radius, startDeg, endDeg, 0)
-    CGContextClosePath(context)
-    CGContextFillPath(context)
-    CGContextRestoreGState(context)
-  }
-
-  func drawSecondsIndicator(context: CGContextRef, color: UIColor, radius: CGFloat, containerRect: CGRect) {
-    let value = CGFloat(seconds) * 6.0
-    let circleCenter = pointFromAngle(value, radius: radius, containerRect: containerRect)
-    let circleRect = CGRectMake(circleCenter.x, circleCenter.y, radius * 2, radius * 2)
-    CGContextSaveGState(context)
-    color.set()
-    CGContextFillEllipseInRect(context, circleRect)
-    CGContextRestoreGState(context)
-  }
-
-  func drawText(context: CGContextRef, rect: CGRect) {
-    var t0 = CGContextGetCTM(context)
-    let xScaleFactor = t0.a > 0 ? t0.a : 0-t0.a
-    let yScaleFactor = t0.a > 0 ? t0.d : 0-t0.d
-    t0 = CGAffineTransformInvert(t0)
-
-    if xScaleFactor != 1.0 || yScaleFactor != 1.0 {
-      t0 = CGAffineTransformScale(t0, xScaleFactor, yScaleFactor)
-    }
-
-    CGContextConcatCTM(context, t0)
-    CGContextSetTextMatrix(context, CGAffineTransformIdentity)
-    let line = CTLineCreateWithAttributedString(attributedString)
-
-
-    let glyphCount = CTLineGetGlyphCount(line)
-    if glyphCount == 0 {
-      return
-    }
-
-    struct GlyphArcInfo {
-      var width: CGFloat
-      var angle: CGFloat
-    }
-  }
-
   func pointFromAngle(angle: CGFloat, radius: CGFloat, containerRect: CGRect)  -> CGPoint {
     let centerPoint = CGPointMake(CGRectGetWidth(frame) / 2 - radius, CGRectGetHeight(frame) / 2 - radius)
     var result = CGPointMake(0.0,0.0)
@@ -376,8 +315,7 @@ public class TimerControl: UIControl {
     let unactiveCircleColor = UIColor(white: 1.0, alpha: 0.4)
 
     if active == true {
-      color = hours > 0
-        ? calculatedColor : normalCircleColor
+      color = hours > 0 ? calculatedColor : normalCircleColor
     } else {
       color = unactiveCircleColor
     }
@@ -605,5 +543,71 @@ public class TimerControl: UIControl {
     }
 
     lastPoint = CGPointZero
+  }
+}
+
+// MARK: - Draw methods
+
+extension TimerControl {
+
+  func drawCircle(context: CGContextRef, color: UIColor, rect: CGRect) {
+    CGContextSaveGState(context)
+    color.set()
+    CGContextFillEllipseInRect(context, rect)
+    CGContextRestoreGState(context)
+  }
+
+  func drawMinutes(context: CGContextRef, color: UIColor, radius: CGFloat, angle: CGFloat, containerRect: CGRect) {
+    CGContextSaveGState(context)
+
+    let angleTranslation: CGFloat = 0 - 90
+    let startDeg: CGFloat = π * (0 + angleTranslation) / 180
+    let endDeg: CGFloat = π * (angle + angleTranslation) / 180
+    let x = CGRectGetWidth(containerRect) / 2 + containerRect.origin.x
+    let y = CGRectGetWidth(containerRect) / 2 + containerRect.origin.y
+
+    color.set()
+
+    CGContextMoveToPoint(context, x, y)
+    CGContextAddArc(context, x, y, radius, startDeg, endDeg, 0)
+    CGContextClosePath(context)
+    CGContextFillPath(context)
+    CGContextRestoreGState(context)
+  }
+
+  func drawSecondsIndicator(context: CGContextRef, color: UIColor, radius: CGFloat, containerRect: CGRect) {
+    let value = CGFloat(seconds) * 6.0
+    let circleCenter = pointFromAngle(value, radius: radius, containerRect: containerRect)
+    let circleRect = CGRectMake(circleCenter.x, circleCenter.y, radius * 2, radius * 2)
+    CGContextSaveGState(context)
+    color.set()
+    CGContextFillEllipseInRect(context, circleRect)
+    CGContextRestoreGState(context)
+  }
+
+  func drawText(context: CGContextRef, rect: CGRect) {
+    var t0 = CGContextGetCTM(context)
+    let xScaleFactor = t0.a > 0 ? t0.a : 0-t0.a
+    let yScaleFactor = t0.a > 0 ? t0.d : 0-t0.d
+    t0 = CGAffineTransformInvert(t0)
+
+    if xScaleFactor != 1.0 || yScaleFactor != 1.0 {
+      t0 = CGAffineTransformScale(t0, xScaleFactor, yScaleFactor)
+    }
+
+    CGContextConcatCTM(context, t0)
+    CGContextSetTextMatrix(context, CGAffineTransformIdentity)
+    let line = CTLineCreateWithAttributedString(attributedString)
+
+
+    let glyphCount = CTLineGetGlyphCount(line)
+    if glyphCount == 0 {
+      return
+    }
+
+    struct GlyphArcInfo {
+      var width: CGFloat
+      var angle: CGFloat
+    }
   }
 }
