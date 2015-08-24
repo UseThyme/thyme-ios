512c9921038c6460960b595ccd0eb1fb4192a41e
Vadym Markov <markov.vadym@gmail.com>
2015-08-25 01:20:21 +0200

    Build alarms response

--- Thyme/Library/Utils/WatchHandler.swift
+++ Thyme/Library/Utils/WatchHandler.swift
@@ -1,9 +1,31 @@
-//
-//  WatchHandler.swift
-//  Thyme
-//
-//  Created by Vadym Markov on 25/08/15.
-//  Copyright (c) 2015 Hyper. All rights reserved.
-//
-
 import Foundation
+
+struct WatchHandler {
+
+  static func response(request: String) -> [NSObject : AnyObject] {
+    var data = [NSObject : AnyObject]()
+
+    if request == "getAlarms" {
+      var notifications = [String]()
+
+      var alarms = [AnyObject]()
+      for index in 0...4 {
+        let section = index == 1 || index == 3  ? 1 : 0
+        let item = index == 2 || index == 3 ? 1 : 0
+        let indexPath = NSIndexPath(forItem: item, inSection: section)
+
+        let alarm = Alarm()
+        alarm.oven = index == 4
+
+        alarms[index] = NSNull()
+        if let notification = LocalNotificationManager.existingNotificationWithAlarmID(alarm.alarmID!) {
+          alarms[index] = notification
+        }
+      }
+
+      data = ["alarms": alarms]
+    }
+
+    return data
+  }
+}
