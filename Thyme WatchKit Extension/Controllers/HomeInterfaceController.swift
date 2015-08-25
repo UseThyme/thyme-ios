67fce868104af1e3c26161447b01088bc2715c7c
Vadym Markov <markov.vadym@gmail.com>
2015-08-25 02:23:59 +0200

    Refactor UI code

--- Thyme WatchKit Extension/Controllers/HomeInterfaceController.swift
+++ Thyme WatchKit Extension/Controllers/HomeInterfaceController.swift
@@ -3,12 +3,26 @@ import Foundation
 
 class HomeInterfaceController: WKInterfaceController {
 
-  @IBOutlet var plateGroups : [WKInterfaceGroup]!
-  @IBOutlet var plateLabels : [WKInterfaceLabel]!
+  @IBOutlet weak var plate1Group: WKInterfaceGroup!
+  @IBOutlet weak var plate2Group: WKInterfaceGroup!
+  @IBOutlet weak var plate3Group: WKInterfaceGroup!
+  @IBOutlet weak var plate4Group: WKInterfaceGroup!
+  @IBOutlet weak var ovenGroup: WKInterfaceGroup!
+
+  @IBOutlet weak var plate1Label: WKInterfaceLabel!
+  @IBOutlet weak var plate2Label: WKInterfaceLabel!
+  @IBOutlet weak var plate3Label: WKInterfaceLabel!
+  @IBOutlet weak var plate4Label: WKInterfaceLabel!
+  @IBOutlet weak var ovenLabel: WKInterfaceLabel!
+
+  var plateGroups = [WKInterfaceGroup]()
+  var plateLabels = [WKInterfaceLabel]()
 
   override func awakeWithContext(context: AnyObject?) {
     super.awakeWithContext(context)
-    loadData()
+
+    plateGroups = [plate1Group, plate2Group, plate3Group, plate4Group, ovenGroup]
+    plateLabels = [plate1Label, plate2Label, plate3Label, plate4Label, ovenLabel]
   }
 
   override func willActivate() {
@@ -28,39 +42,48 @@ class HomeInterfaceController: WKInterfaceController {
       if let response = response,
         alarms = response["alarms"] as? [AnyObject] {
           for (index, alarm) in enumerate(alarms) {
-            if let notification = alarm as? UILocalNotification,
-              userinfo = notification.userInfo,
-              firedDate = userinfo["HYPAlarmFireDate"] as? NSDate,
-              numberOfSeconds = userinfo["HYPAlarmFireInterval"] as? NSNumber
+            if let alarmData = alarm as? [String: AnyObject]
               where index < self.plateGroups.count {
-                let secondsPassed: NSTimeInterval = NSDate().timeIntervalSinceDate(firedDate)
-                let secondsLeft = NSTimeInterval(numberOfSeconds.integerValue) - secondsPassed
-                let currentSecond = secondsLeft % 60
-                var minutesLeft = floor(secondsLeft / 60)
-                let hoursLeft = floor(minutesLeft / 60)
-
-                if hoursLeft > 0 {
-                  minutesLeft = minutesLeft - (hoursLeft * 60)
-                }
-
-                var text = "\(minutesLeft)"
-                if hoursLeft > 0 {
-                  if minutesLeft < 10 {
-                    text = "\(hoursLeft):0\(minutesLeft)"
-                  } else {
-                    text = "\(hoursLeft):\(minutesLeft)"
-                  }
-                }
-
-                self.plateGroups[index].setBackgroundImageNamed("timerFrame")
-                self.plateGroups[index].startAnimatingWithImagesInRange(NSRange(location: Int(minutesLeft), length: 1),
-                  duration: 0, repeatCount: 1)
-                self.plateLabels[index].setText(text)
+                self.updatePlate(index, data: alarmData)
             }
           }
       } else {
         println("Error with fetching of alarms from the parent app")
       }
+    }
+  }
+
+  // MARK: - UI
+
+  func updatePlate(index: Int, data: [String: AnyObject]) {
+    if let firedDate = data["firedDate"] as? NSDate,
+      numberOfSeconds = data["numberOfSeconds"] as? NSNumber {
+        let secondsPassed: NSTimeInterval = NSDate().timeIntervalSinceDate(firedDate)
+        let secondsLeft = NSTimeInterval(numberOfSeconds.integerValue) - secondsPassed
+        let currentSecond = secondsLeft % 60
+        var minutesLeft = floor(secondsLeft / 60)
+        let hoursLeft = floor(minutesLeft / 60)
+
+        if hoursLeft > 0 {
+          minutesLeft = minutesLeft - (hoursLeft * 60)
+        }
+
+        let minutes = Int(minutesLeft)
+        let hours = Int(hoursLeft)
+        var text = "\(minutesLeft)"
+
+        if hours > 0 {
+          if minutes < 10 {
+            text = "\(hours):0\(minutes)"
+          } else {
+            text = "\(hours):\(minutes)"
+          }
+        }
+
+        self.plateGroups[index].setBackgroundImageNamed("timerFrame")
+        self.plateGroups[index].startAnimatingWithImagesInRange(NSRange(location: minutes, length: 1),
+          duration: 0, repeatCount: 1)
+        self.plateLabels[index].setText(text)
     }
   }
 }
