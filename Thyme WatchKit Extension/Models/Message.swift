8ea970e288e6ebd609a1bfbfcbb2f18b998d7113
Vadym Markov <markov.vadym@gmail.com>
2015-09-05 17:10:09 +0200

    Move Message to models

---
+++ Thyme WatchKit Extension/Models/Message.swift
@@ -0,0 +1,29 @@
+import WatchKit
+import Foundation
+import WatchConnectivity
+
+struct Message {
+
+  enum Kind: String {
+    case GetAlarms = "getAlarms"
+    case CancelAlarms = "cancelAlarms"
+    case GetAlarm = "getAlarm"
+    case UpdateAlarmMinutes = "updateAlarmMinutes"
+    case CancelAlarm = "cancelAlarm"
+  }
+
+  var kind: Kind
+  var parameters: [String: AnyObject]
+
+  var data: [String: AnyObject] {
+    var data = parameters
+    data["request"] = kind.rawValue
+
+    return data
+  }
+
+  init(_ kind: Kind, _ parameters: [String: AnyObject] = [:]) {
+    self.kind = kind
+    self.parameters = parameters
+  }
+}
