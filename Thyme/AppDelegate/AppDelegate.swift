7bb180a16545e350266096790fd67e4124ec09cb
Christoffer Winterkvist <chris@zenangst.com>
2015-09-05 11:01:40 +0200

    Remove newlines

--- Thyme/AppDelegate/AppDelegate.swift
+++ Thyme/AppDelegate/AppDelegate.swift
@@ -35,7 +35,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, BITHockeyManagerDelegate,
 
   lazy var homeController: HomeViewController = {
     var theme: Themable = Theme.Main()
-
     if UIAccessibilityDarkerSystemColorsEnabled() {
       theme = Theme.DarkColors()
 
@@ -115,7 +114,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, BITHockeyManagerDelegate,
     homeController.setNeedsStatusBarAppearanceUpdate()
 
     if AlarmCenter.notificationsSettings().types != UIApplication.sharedApplication().currentUserNotificationSettings()?.types {
-      
       if !homeController.herbieController.isBeingPresented() {
         let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(1 * Double(NSEC_PER_SEC)))
         dispatch_after(delayTime, dispatch_get_main_queue()) {
