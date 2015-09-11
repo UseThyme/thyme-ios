0e0a1eac619a378b65d7678b0c5cc2b40d027948
Vadym Markov <markov.vadym@gmail.com>
2015-09-11 14:37:26 +0200

    Remove connection lost animation

---
+++ Thyme WatchKit Extension/ExtensionDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  ExtensionDelegate.swift
+//  Thyme
+//
+//  Created by Vadym Markov on 11/09/15.
+//  Copyright © 2015 Hyper. All rights reserved.
+//
+
+import WatchKit
+
+class ExtensionDelegate: NSObject {
+
+}
