6b3647715318fd62b0cca1e7f60fe60bb9d624d9
Elvis Nuñez <elvisnunez@me.com>
2018-06-09 20:55:50 +0100

    Remove unused

--- ThymeTests/ThymeTests.m
+++ ThymeTests/ThymeTests.m
@@ -1,29 +1,9 @@
-#import <XCTest/XCTest.h>
+@import XCTest;
 
 @interface ThymeTests : XCTestCase
 
 @end
 
 @implementation ThymeTests
-
-- (void)setUp
-{
-    [super setUp];
-}
-
-- (void)tearDown
-{
-    [super tearDown];
-}
-
-- (NSNumber *)minutesForHours:(NSInteger)hours andMinutes:(NSInteger)minutes
-{
-    return @(hours * 60 + minutes);
-}
-
-- (NSNumber *)secondsForHours:(NSInteger)hours minutes:(NSInteger)minutes seconds:(NSInteger)seconds
-{
-    return @(hours * 3600 + minutes * 60 + seconds);
-}
 
 @end
